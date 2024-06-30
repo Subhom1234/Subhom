8b7504a0828a60ef6c0fb17b5ce28beca88cfa75
Subhom1234 <147125659+Subhom1234@users.noreply.github.com>
2024-06-30 12:20:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function r1(){
 }
  
  
- {
+ function submit(){
  const scriptURL = 'https://script.google.com/macros/s/AKfycby-OWRrE7kYDqJ6a5vHVLkeyl69Mxkux0lIu1Eg4L3xFIcRmkUiV2E85x1uHSQoCldSiA/exec'
   const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
@@ -27,3 +27,20 @@ const msg = document.getElementById("msg")
       .catch(error => console.error('Error!', error.message))
   })
 }
+function submit_mobile(){
+  const scriptURL = 'https://script.google.com/macros/s/AKfycby-OWRrE7kYDqJ6a5vHVLkeyl69Mxkux0lIu1Eg4L3xFIcRmkUiV2E85x1uHSQoCldSiA/exec'
+   const form = document.forms['submit-to-google-sheet1']
+ const msg1 = document.getElementById("msg1")
+   form.addEventListener('submit', e => {
+     e.preventDefault()
+     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+       .then(response => {
+       msg1.innerHTML = "Message sent successfully"
+       setTimeout(function(){
+       msg1.innerHtml = ""
+       },5)
+       form.reset()
+       })
+       .catch(error => console.error('Error!', error.message))
+   })
+ }
